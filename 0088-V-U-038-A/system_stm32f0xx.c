7b61b3743866de6fdaacffd25a824c24e0fc6c03
EdKung <edkung0728@gmail.com>
2015-07-13 16:17:45 +0800

    before pulling

--- 0088-V-U-038-A/system_stm32f0xx.c
+++ 0088-V-U-038-A/system_stm32f0xx.c
@@ -317,6 +317,7 @@ static void SetSysClock(void)
     RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE_DIV1;
 
     /* PLL configuration = HSE * 6 = 48 MHz */
+		/* PLL configuration = HSE(16MHz) * 3 = 48 MHz (FCM2401) */  
     RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
     RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_PREDIV1 | RCC_CFGR_PLLXTPRE_PREDIV1 | RCC_CFGR_PLLMULL6);
             
